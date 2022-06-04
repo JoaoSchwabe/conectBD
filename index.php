b0efd875ee506497fced1debe5f797b5b70215c5
João Gabriel Schwabe <81689363+JoaoSchwabe@users.noreply.github.com>
2022-06-03 21:07:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+function phpconnect(){
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $con = mysqli_connect($host, $user, $pass);
+    $res = $con->query("SHOW TABLES FROM exercicio1");
+
+
+    if (!$con) {
+        die("Falha na conexão: " . mysqli_connect_errno());
+        return false;
+    } else {
+        echo "Conexão realizada com sucesso<br>";
+        print_r($res);
+        return $res;
+    }
+}   
+phpconnect();
+
